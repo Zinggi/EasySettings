004d4e4c922c28eebd781725cde31c8cbdb5ec94
Zinggi <schrott57bla@gmail.com>
2013-04-16 23:08:51 +0200

    Initial commit

---
+++ EasySettings.py
@@ -0,0 +1,114 @@
+#-----------------------------------------------------------------------------------
+# Easy Settings
+#-----------------------------------------------------------------------------------
+#
+# This Plug-in helps you with editing the Sublime Text 2 setting files.
+# It displays all available settings through auto-completion.
+# It also displays the documentation of the just accepted setting.
+#
+# (c) Florian Zinggeler
+#-----------------------------------------------------------------------------------
+import sublime
+import sublime_plugin
+import json
+import os
+
+
+class EasySettings(sublime_plugin.EventListener):
+    comments = []
+    settings = {}
+    b_did_autocomplete = False
+    b_helper_panel_on = False
+    help_panel_line_number = -1
+
+    def is_setting_file(self, view):
+        if view and view.file_name():
+            return view.file_name().split('.')[-1] == "sublime-settings"
+        return False
+
+    def on_activated(self, view):
+        if self.is_setting_file(view):
+            base_name = self.find_base_file(view.file_name().split('\\')[-1])
+            self.settings = self.parse_setting(base_name)
+
+    # search for the base settings file
+    def find_base_file(self, filename):
+        for root, dirs, files in os.walk(sublime.packages_path()):
+            if filename in files:
+                if os.path.join(root, filename).split('\\')[-2] != "User":
+                    print "found: %s" % os.path.join(root, filename)
+                    return os.path.join(root, filename)
+
+    # parse the setting file and capture comments
+    def parse_setting(self, filename):
+        current_comment = ""
+        with open(filename) as f:
+            content = ""
+            for line in f:
+                if "//" == line.strip()[:2]:
+                    current_comment += line
+                else:
+                    content += line
+                    if current_comment != "":
+                        current_comment += line
+                        self.comments.append(current_comment)
+                        current_comment = ""
+            # Return json file
+            return json.loads(content)
+
+    # gets called when auto-completion pops up.
+    def on_query_completions(self, view, prefix, locations):
+        if self.is_setting_file(view):
+            return self.get_autocomplete_list(prefix)
+
+    # gets called when auto-completion is triggered
+    def on_query_context(self, view, key, operator, operand, match_all):
+        if self.is_setting_file(view):
+            if key == "show_documentation":
+                region = view.sel()[0]
+                if region.empty():
+                    self.b_did_autocomplete = True
+
+    # remove auto completion and insert dynamic snippet instead, just after auto completion
+    def on_modified(self, view):
+        if self.is_setting_file(view):
+            # if the helper panel has just been displayed, save the line number
+            if self.b_helper_panel_on:
+                self.help_panel_line_number = view.rowcol(view.sel()[0].begin())[0]
+                self.b_helper_panel_on = False
+
+            elif self.help_panel_line_number != -1:
+                # if we are modifying anything above or below the helper panel line, hide the panel.
+                if view.rowcol(view.sel()[0].begin())[0] != self.help_panel_line_number:
+                    view.window().run_command("hide_panel", {"panel": "output.settings_documentation_panel"})
+                    self.help_panel_line_number = -1
+
+            if self.b_did_autocomplete:
+                self.b_did_autocomplete = False
+                region_line = view.line(view.sel()[0])
+                word = view.substr(region_line).strip()
+                panel = view.window().get_output_panel('settings_documentation_panel')
+                panel_edit = panel.begin_edit()
+                panel.insert(panel_edit, panel.size(), self.get_documentation_for(word))
+                panel.end_edit(panel_edit)
+                panel.show(panel.size())
+                view.window().run_command("show_panel", {"panel": "output.settings_documentation_panel"})
+                self.b_helper_panel_on = True
+
+    # This will return all settings found in the base settings
+    def get_autocomplete_list(self, word):
+        autocomplete_list = []
+
+        for s in self.settings:
+            autocomplete_list.append((s + "\tsetting", "\"" + s + "\": "))
+
+        return autocomplete_list
+
+    def get_documentation_for(self, word):
+        print word
+        for c in self.comments:
+            print c.split("\n")[-2]
+            if word in c.split("\n")[-2]:
+                print c
+                return c
+        return ""
---
+++ README.md
@@ -0,0 +1,48 @@
+Easy Settings
+========================
+
+![Pic](http://www.mediafire.com/convkey/5170/5mlumwsbc5ziulv6g.jpg)
+
+This Plug-in helps you with editing the Sublime Text 2 setting files.  
+It displays all available settings through auto-completion.  
+It also displays the documentation of the just accepted setting.  
+
+Installation
+------------
+**Not available yet**
+<pre>
+**Very easy with [Package Control](http://wbond.net/sublime_packages/package_control) right inside Sublime Text 2 (Package Control needs to be installed):**
+
+1.	Ctrl + shift + P
+2.  Search for "inst", hit enter
+3.  Search for "DictionaryAutoComplete", hit enter</pre>
+
+**Manually (not recommended):**
+
+1.  Clone or download this package
+2.	Put it into your Packages directory (find using 'Preferences' -> 'Browse Packages...')
+
+
+
+I'm open to donations :wink:  
+[![Donate](https://www.paypalobjects.com/en_GB/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XT5LYESK99ESA)
+
+
+* * *
+License
+------------
+Easy Settings for Sublime Text 2
+Copyright (C) 2013 Florian Zinggeler
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
